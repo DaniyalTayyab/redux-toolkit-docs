cc366b074eedd0cdc836152e635c4506ac0a8ab3
daniyaltayyab <daniyaltayyab91@gmail.com>
2022-10-19 19:43:38 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { incremented, amountAdded } from "./features/counter/counterSlice";
+import { useFetchBreedsQuery } from "./features/dogs/dogApiSlice";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState(10);
+
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
+
+  const handleClick = () => {
+    dispatch(amountAdded(3));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App">
+        <button onClick={handleClick}>count is: {count}</button>
+      </div>
+      <div>
+        <p>Dogs to fetch:</p>
+        <select
+          value={numDogs}
+          onChange={(e) => setNumDogs(Number(e.target.value))}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+      </div>
+      <div>
+        <p>Number of dogs fetched: {data.length}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((breed) => (
+              <tr>
+                <td>{breed.name}</td>
+                <td>
+                  <img src={breed.image.url} height={250} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
